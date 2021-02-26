1b5671e74609d1c06d2ce1c628599e5e31f979d1
w91379137 <w913791379137@gmail.com>
2021-02-27 02:08:20 +0800

    add middleware

--- src/app.py
+++ src/app.py
@@ -2,6 +2,21 @@
 from server import server
 
 
+# middleware
+# https://stackoverflow.com/questions/51691730/flask-middleware-for-specific-route
+from flask import request
+import json
+@server.before_request
+def hook():
+    # request - flask.request
+    print(
+        f"""
+        [{request.method}]
+        {request.url} {request.path}
+        args: {json.dumps(request.args)}
+        data: {request.data}
+        [{request.endpoint}] """
+    )
 
 if __name__ == "__main__":
     server.run(host = "0.0.0.0", port = 5000, debug = True)
