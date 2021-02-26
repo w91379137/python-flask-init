f0398e973a7d7572ee225961fb5dad9786a50cf6
w91379137 <w913791379137@gmail.com>
2021-02-27 02:03:31 +0800

    新增 post

--- src/server/controller/root/__init__.py
+++ src/server/controller/root/__init__.py
@@ -4,7 +4,9 @@ from flask import Blueprint
 api = Blueprint('root', __name__)
 
 # curl http://localhost:5000/sayhello
+# curl -X GET http://localhost:5000/sayhello
+# curl -X POST --data "enter somethong"  http://localhost:5000/sayhello
 from .sayhello import sayhello as _sayhello
-@api.route('/sayhello', methods=['GET'])
+@api.route('/sayhello', methods=['GET', 'POST'])
 def sayhello():
     return _sayhello(**locals())
