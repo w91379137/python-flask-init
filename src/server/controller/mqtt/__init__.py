14aa07681da7a98f3c7fbbe7b930cbb5f95aabf6
w91379137 <w913791379137@gmail.com>
2021-02-27 14:20:31 +0800

    新增 publish api

--- src/server/controller/mqtt/__init__.py
+++ src/server/controller/mqtt/__init__.py
@@ -13,3 +13,14 @@ from .subscribe import subscribe as _subscribe
 @api.route('/subscribe', methods=['GET', 'POST'])
 def subscribe():
     return _subscribe(**locals())
+
+
+'''
+$ curl -X GET \
+    'http://localhost:5000/mqtt/publish?topic=topic1&msg=hello'
+'''
+
+from .publish import publish as _publish
+@api.route('/publish', methods=['GET', 'POST'])
+def publish():
+    return _publish(**locals())
---
+++ src/server/controller/mqtt/publish.py
@@ -0,0 +1,12 @@
+
+from service import main
+from flask import request
+from ..tool import getValueInArgBody
+
+def publish():
+    topic = getValueInArgBody(request, 'topic')
+    msg = getValueInArgBody(request, 'msg')
+    if topic is not None:
+        main.mqtt.publish(topic, msg)
+
+    return f'{topic}: {msg}'
