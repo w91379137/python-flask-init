5721ee8716edb22cd33197ee59da1aaf77d7a961
w91379137 <w913791379137@gmail.com>
2021-10-28 17:40:45 +0800

    [feat] 修正 server 啟動 host port

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 
 from asabulu.view import app
+from asabulu.service import main
 
 app.run(host ='0.0.0.0', port = 5000, threaded=True)
+
+if __name__ == "__main__":
+    config = main.config.server
+    app.run(host = config.host, port = config.port, debug = True)
--- asabulu/view.py
+++ asabulu/view.py
@@ -1,4 +1,9 @@
 
 from asabulu.util import create_app
+from asabulu.service import main
 
 app = create_app()
+
+if __name__ == "__main__":
+    config = main.config.server
+    app.run(host = config.host, port = config.port, debug = True)
