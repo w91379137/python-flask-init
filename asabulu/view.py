9221c7150528aaa21779e9f87edc3f798e7a6505
w91379137 <w913791379137@gmail.com>
2021-10-29 09:32:07 +0800

    [fix] 修正 app 重複啟動

--- asabulu/view.py
+++ asabulu/view.py
@@ -2,8 +2,8 @@
 from asabulu.util import create_app
 from asabulu.service import main
 
-app = create_app()
+app_instance = create_app()
 
 if __name__ == "__main__":
     config = main.config.server
-    app.run(host = config.host, port = config.port, debug = True)
+    app_instance.run(host = config.host, port = config.port, debug = True)
